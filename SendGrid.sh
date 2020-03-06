b9d614ff50f8d87b34a72a359497ccda8a41d31a
TS0601 <51117758+TS0601@users.noreply.github.com>
2020-03-06 12:18:18 +0900

    Add files via upload

---
+++ SendGrid.sh
@@ -0,0 +1,1 @@
+curl -X POST https://api.sendgrid.com/v3/mail/send -d @SendGridテスト.json -H "Authorization: Bearer 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"  -H "Content-Type: application/json"
